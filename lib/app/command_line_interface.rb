37c9ca633684aa6ede4b8afab6dc649f3e80b5d8
Khang <khang.le27@gmail.com>
2019-03-05 10:53:16 +0000

    added #gets_pokemon_user_selection

--- lib/app/command_line_interface.rb
+++ lib/app/command_line_interface.rb
@@ -23,11 +23,37 @@ end
 
 def pokemon_selection(user)
   pokemon_selection_message
-  pokemon_list_formatter
+  #pokemon_selection_loop
 end
 
 def pokemon_selection_message
   puts "You will be choosing 3 companions for your battlepack to help you progress through your journey as a poke-trainer. Here's a list of available pokemons."
+end
+
+def pokemon_selection_loop(user)
+  while battlepack_count(user) < 3
+    battlepack_space_message(user)
+    pokemon_list_formatter
+  end
+end
+
+def battlepack_space_message(user)
+  puts "You have #{3-battlepack_count(user)} spaces left in your battlepack."
+end
+
+def gets_pokemon_user_selection
+  puts "Pick a pokemon"
+  input = gets.chomp.capitalize
+  if pokemon_list.include?(input)
+    return input
+  else
+    puts "Invalid pokemon name selection."
+    gets_pokemon_user_selection
+  end
+end
+
+def create_battlepack_instance(user)
+
 end
 
 def pokemon_list_formatter
@@ -42,8 +68,6 @@ def battlepack_count(user)
   binding.pry
   user.battle_packs.count
 end
-
-
 
 
 
