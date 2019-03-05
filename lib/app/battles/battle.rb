77669d0dea81b63091e7074a5a118c2d76e6cdd8
github username <github email address>
2019-03-05 16:22:55 +0000

    started main battle function

--- lib/app/battles/battle.rb
+++ lib/app/battles/battle.rb
@@ -1,5 +1,15 @@
-current = User.all[0].battle_packs[0] #instance of BattlePack
-target = User.all[1].battle_packs[0] #instance of BattlePack
+left = User.all[0].battle_packs[0] #instance of BattlePack
+right = User.all[1].battle_packs[0] #instance of BattlePack
+
+def battle(left, right)
+  current = left
+  target = right
+  battle_intro(current, target)
+  while current.pokemon.hp > 0 && target.pokemon.hp > 0
+    battle_turn(current, target)
+    current, target = target, current
+  end
+end
 
 def battle_intro(current, target)
   puts "It's #{current.user.username.capitalize}'s #{current.pokemon.name} VS. #{target.user.username.capitalize}'s #{target.pokemon.name}!!!!"
@@ -7,18 +17,23 @@ end
 
 def battle_turn(current, target)
   puts "It's #{current.pokemon.name}'s turn!'"
-  move_selection = get_move_selection(current)
-  move = current.pokemon.moves[move_selection-1]
+  move = get_move_selection(current)
   current.pokemon.attack_target(move, target.pokemon)
-  binding.pry
+end
+
+
+def puts_stats(*battle_packs)
+  battle_packs.each do |battle_pack|
+    puts "#{battle_pack.pokemon.name}, hp: #{battle_pack.pokemon.hp}"
+  end
 end
 
 def get_move_selection(current)
   puts "What would you like to do? Choose the number linked to the move"
   moves_list_formatted(current.pokemon)
   input = STDIN.gets.chomp.to_i
-  if (1..4).include?(input)
-    return input
+  if (1..4).include?(input) && current.pokemon.moves[input-1].pp > 0
+    current.pokemon.moves[input-1]
   else
     puts "Invalid selection."
     get_move_selection
@@ -26,11 +41,5 @@ def get_move_selection(current)
 end
 
 def moves_list_formatted(pokemon)
-  moves_list(pokemon).each_with_index { |move, index| puts "#{index + 1}. #{move}"}
+  pokemon.moves.each_with_index { |move, index| puts "#{index + 1}. #{move.name}, pp: #{move.pp}"}
 end
-
-def moves_list(pokemon)
-  pokemon.moves.map(&:name)
-end
-
-current_pokemon, target_pokemon = target_pokemon, current_pokemon
--- lib/app/models/pokemon.rb
+++ lib/app/models/pokemon.rb
@@ -5,22 +5,34 @@ class Pokemon < ActiveRecord::Base
   has_many :moves, through: :movesets
 
   def attack_target(move, target)
+    if rand(0..100) < move.accuracy
+      hit(move, target)
+    else
+      move.pp -= 1
+      puts "It missed!!"
+    end
+  end
+
+  def hit(move, target)
     damage = damage_calc(move, target)
     new_hp = target.hp - damage
     if new_hp < 0
       target.hp = 0
     else
       target.hp = new_hp
+      move.pp -= 1
+      puts "It did #{damage} damage!"
     end
-    move.pp -= 1
   end
 
   def damage_calc(move, target)
     case target.pokemon_type
     when move.effective_against
       mod = 2 * rand(0.9..1)
+    puts "It's super effective!!!"
     when move.not_effective_against
       mod = 0.5 * rand(1..1.1)
+      put "It's not very effective..."
     else
       mod = 1 * rand(0.95..1.05)
     end
