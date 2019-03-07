030db2280bf67d3417c408eff4f7d48cf61959e5
github username <github email address>
2019-03-07 12:54:16 +0000

    images work

--- lib/app/battle/battle_screen.rb
+++ lib/app/battle/battle_screen.rb
@@ -2,19 +2,21 @@ def generate_screen(left, right)
   terminal_width = IO.console.winsize[1]
 
   # right pokemon
+  right_image = right.pokemon.image
   right_name = right.pokemon.name.capitalize
   right_current_hp = right.pokemon.hp
   right_starting_hp = right.pokemon.starting_hp
   right_health_bar = health_bar(right.pokemon.hp, right.pokemon.starting_hp)
 
-  Catpix.print_image("lib/images/pikachu.png", limit_y: 0.4, center_x: true)
+  Catpix.print_image(right_image, limit_y: 0.4, center_x: true)
   puts right_name.rjust(terminal_width/2)
   puts "hp: #{right_current_hp}/#{right_starting_hp}".rjust(terminal_width/2)
   puts right_health_bar.rjust(terminal_width/2)
 
   # left pokemon
+  left_image = left.pokemon.image
   left_health_bar = health_bar(left.pokemon.hp, left.pokemon.starting_hp)
-  Catpix.print_image("lib/images/pikachu.png", limit_y: 0.4)
+  Catpix.print_image(left_image, limit_y: 0.4)
   puts "#{left.pokemon.name.capitalize}\nhp: #{left.pokemon.hp}/#{left.pokemon.starting_hp}\n#{left_health_bar}\n"
   puts '-' * terminal_width
 end
