180d8b9b929602b8806ec08648f16ae571040958
github username <github email address>
2019-03-07 10:17:42 +0000

    added starting hp and image to pokemons table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190304144131) do
+ActiveRecord::Schema.define(version: 20190307101633) do
 
   create_table "battle_packs", force: :cascade do |t|
     t.integer "user_id"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20190304144131) do
     t.integer "hp"
     t.integer "attack"
     t.integer "defence"
+    t.integer "starting_hp"
+    t.string  "image"
   end
 
   create_table "users", force: :cascade do |t|
