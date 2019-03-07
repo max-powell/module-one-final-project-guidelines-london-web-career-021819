a6ee92ca0571cbcef423aaac2b2c1ac98bfba4f7
github username <github email address>
2019-03-07 12:16:19 +0000

    fixed battle + generates screen

--- lib/app/battle/battle_screen.rb
+++ lib/app/battle/battle_screen.rb
@@ -7,25 +7,25 @@ def generate_screen(left, right)
   right_starting_hp = right.pokemon.starting_hp
   right_health_bar = health_bar(right.pokemon.hp, right.pokemon.starting_hp)
 
-  Catpix.print_image("lib/images/pikachu-f.png", limit_y: 0.4, center_x: true)
+  Catpix.print_image("lib/images/pikachu.png", limit_y: 0.4, center_x: true)
   puts right_name.rjust(terminal_width/2)
   puts "hp: #{right_current_hp}/#{right_starting_hp}".rjust(terminal_width/2)
   puts right_health_bar.rjust(terminal_width/2)
 
   # left pokemon
   left_health_bar = health_bar(left.pokemon.hp, left.pokemon.starting_hp)
-  Catpix.print_image("lib/images/pikachu-f.png", limit_y: 0.4)
+  Catpix.print_image("lib/images/pikachu.png", limit_y: 0.4)
   puts "#{left.pokemon.name.capitalize}\nhp: #{left.pokemon.hp}/#{left.pokemon.starting_hp}\n#{left_health_bar}\n"
   puts '-' * terminal_width
 end
 
 def health_bar(current_hp, starting_hp)
-  filled = health(current_hp, starting_hp)/5
+  filled = health_for_bar(current_hp, starting_hp)/5
   empty = 20 - filled
   "[#{'=' * filled}#{' ' * empty}]"
 end
 
-def health(current_hp, starting_hp)
+def health_for_bar(current_hp, starting_hp)
   health = (current_hp.to_f/starting_hp)*100
   if health % 5 > 0
     health -= health % 5
